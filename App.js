fe2e047829a202d3175fbe22a49fcb98432b5535
NCJo <cnimkulr@gmail.com>
2019-05-13 17:21:38 -0600

    Added: draw a simple box on the screen

--- App.js
+++ App.js
@@ -1,49 +1,25 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, StatusBar} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import Matter from "matter-js"
+import { GameEngine } from "react-native-game-engine";
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
+    const { width, height } = Dimensions.get("screen");
+    const boxSize = Math.trunc(Math.max(width, height) * 0.075);
+    const initialBox = Matter.Bodies.rectangle(width / 2, height / 2, boxSize, boxSize);
+
+    <GameEngine style={StyleSheet.container}>
+      <StatusBar hidden={true} />
+    </GameEngine>
   }
 }
 
-const styles = StyleSheet.create({
+const styles = Styles.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    flex:1,
+    backgroundColor: '#fff',
   },
 });
