8b8c717705f1a784d59b642e07e8274d96fcd533
NCJo <cnimkulr@gmail.com>
2019-05-13 19:37:32 -0600

    Added: physics engine

--- App.js
+++ App.js
@@ -1,23 +1,60 @@
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, StatusBar} from 'react-native';
+import {Platform, StyleSheet, StatusBar, Dimensions} from 'react-native';
 
 import Matter from "matter-js"
 import { GameEngine } from "react-native-game-engine";
 
+import Box from './Box';
+
+
+const { width, height } = Dimensions.get("screen");
+const boxSize = Math.trunc(Math.max(width, height) * 0.075);
+const initialBox = Matter.Bodies.rectangle(width / 2, height / 2, boxSize, boxSize);
+const floor = Matter.Bodies.rectangle(width / 2, height - boxSize / 2, width, boxSize, { isStatic: true });
+
+const engine = Matter.Engine.create({ enableSleeping: false });
+const world = engine.world;
+
+Matter.World.add(world, [initialBox, floor]);
+
+const Physics = (entities, { time }) => {
+  let engine = entities["physics"].engine;
+  Matter.Engine.update(engine, time.delta);
+  return entities;
+};
+
+
 export default class App extends Component {
   render() {
-    const { width, height } = Dimensions.get("screen");
-    const boxSize = Math.trunc(Math.max(width, height) * 0.075);
-    const initialBox = Matter.Bodies.rectangle(width / 2, height / 2, boxSize, boxSize);
-
-    <GameEngine style={StyleSheet.container}>
-      <StatusBar hidden={true} />
-    </GameEngine>
+    return (
+      <GameEngine 
+      style={styles.container}
+      entities={{
+        physics: {
+          engine: engine,
+          world: world
+        },
+        initialBox: {
+          body: initialBox,
+          size: [boxSize, boxSize],
+          color: 'red',
+          renderer: Box
+        },
+        floor: {
+          body: floor,
+          size: [width, boxSize],
+          color: "green",
+          renderer: Box
+        } 
+      }}>
+        <StatusBar hidden={true} />
+      </GameEngine>
+    );
   }
 }
 
-const styles = Styles.create({
+const styles = StyleSheet.create({
   container: {
     flex:1,
     backgroundColor: '#fff',
